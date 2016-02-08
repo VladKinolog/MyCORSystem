8289f8b463a8bc7417a39cc82e6952cf21afea43
Vladyslav_Kovega <8viper4@gmail.com>
2016-02-08 17:18:05 +0200

    Изменение цвета через меню.

--- app/src/main/java/com/example/vladyslav_kovega/mycorsystem/SettingActivity.java
+++ app/src/main/java/com/example/vladyslav_kovega/mycorsystem/SettingActivity.java
@@ -1,8 +1,12 @@
 package com.example.vladyslav_kovega.mycorsystem;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+        import android.app.Activity;
+        import android.content.res.Resources;
+        import android.os.Bundle;
+        import android.support.v4.content.ContextCompat;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.LinearLayout;
 
 /**
  * Created by Vladyslav
@@ -15,5 +19,44 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.prog_setting);
+            //TODO Осуществить проверку на первый запуск.??
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.green_bg));
+    }
+
+    public void onClickGreenBgButton(View view){
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.green_bg));
+
+    }
+
+    public void onClickBlueBgButton(View view){
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.blue_bg));
+
+    }
+
+    public void onClickLightyellowBgButton(View view){
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.light_yel_bg));
+
+    }
+
+    public void onClickYellowBgButton(View view){
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.yellow_bg));
+
+    }
+
+    public void onClickPinkBgButton(View view){
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.pink_bg));
+
+    }
+
+    public void onClickRedBgButton(View view) {
+        LinearLayout linearLayoutRoot = (LinearLayout) findViewById(R.id.activity_root_element);
+        linearLayoutRoot.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.red_bg));
+
     }
 }
